88a381458cda125a97d9c68b6237fd707b0ce9ee
concerned citizen <85201967+citizen00147@users.noreply.github.com>
2022-03-02 13:14:54 -0500

    update the mongo connection

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 mongoose.connect(
-  `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_CLUSTER}.mongodb.net/${process.env.MONGO_DATABASE}?authSource=admin`
+  `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER}.mongodb.net/${process.env.MONGO_DATABASE}?authSource=admin`
 );
 
 const db = mongoose.connection;
